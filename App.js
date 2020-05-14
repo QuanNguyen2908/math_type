75ecbea9ab3db362b530d52b51bcbd59682de1f9
Quan <quan.nguyen.2629@gmail.com>
2020-05-14 18:00:36 +0700

    commit

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, Dimensions ,FlatList} from 'react-native';
 import MathView from 'react-native-math-view';
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
+
+const window = Dimensions.get('window');
+
 const App = () => {
   const Login = () => {
     const data = {email: 'admin@opentechiz.com', password: 'admin123'};
@@ -16,45 +19,97 @@ const App = () => {
       });
   };
   const [dataQuestion, setDataQuestion] = useState('');
+  const [idSubject, setIdSubject] = useState('');
+
   const LoadQuestion = async () => {
     const token = await AsyncStorage.getItem('token');
     const config = {headers: {Authorization: `Bearer ${token}`}};
-    axios
-      .get(
-        'https://api.opentechiz-cert.just4demo.biz/api/question/list',
-        config,
-      )
-      .then(res => {
-        setDataQuestion(res.data);
-      });
+    const data = {name: 'Toán'}
+    const response = await axios.post('https://api.opentechiz-cert.just4demo.biz/api/subject/name', data, config)
+    const dataQ = await axios.post('https://api.opentechiz-cert.just4demo.biz/api/question/subject', {ID : response.data.ID}, config)
+    setDataQuestion(dataQ.data)
+    console.log (dataQ.data)
+    // axios
+    //   .get(
+    //     'https://api.opentechiz-cert.just4demo.biz/api/question/list',
+    //     config,
+    //   )
+    //   .then(res => {
+    //     setDataQuestion(res.data);
+    //   });
   };
   useEffect(() => {
     Login();
     LoadQuestion();
   }, []);
-  console.log(dataQuestion);
-  const renderQuestion = () => {
-    return (
-      <View>
-        {dataQuestion !== '' &&
-          dataQuestion.map((item, index) => {
-            const dataArr = item.question.split('`');
-            return dataArr.map((item, index) => {
-              if (item.indexOf('\\') !== -1) {
-                console.log(item);
-                return <MathView math={item} key={index} />;
-              } else {
-                console.log(item);
-                return <Text key={index}>{item}</Text>;
-              }
-            });
-          })}
-      </View>
-    );
+  // const renderQuestion = () => {
+  //   return (
+  //     <View>
+  //       {dataQuestion !== '' &&
+  //         dataQuestion.map((item, index) => {
+  //           const dataArr = item.question.split('`');
+  //           return (
+  //             <View style={styles.viewQuestion} key={index}>
+  //               <Text>Câu {index+1} : </Text>
+  //               {dataArr.map((item, index) => {
+  //                 if (item.indexOf('\\') !== -1) {
+  //                   return <MathView math={item} key={index} />;
+  //                 } else {
+  //                   return <Text key={index}>{item}</Text>;
+  //                 }
+  //               })}
+  //             </View>
+  //           );
+  //         })}
+  //     </View>
+  //   );
+
+  const renderQuestion = (item) => {
+    console.log(item)
+    // return (
+      
+    //   <View>
+    //     {/* {dataQuestion !== '' &&
+    //       dataQuestion.map((item, index) => {
+    //         const dataArr = item.question.split('`');
+    //         return (
+    //           <View style={styles.viewQuestion} key={index}>
+    //             <Text>Câu {index+1} : </Text>
+    //             {dataArr.map((item, index) => {
+    //               if (item.indexOf('\\') !== -1) {
+    //                 return <MathView math={item} key={index} />;
+    //               } else {
+    //                 return <Text key={index}>{item}</Text>;
+    //               }
+    //             })}
+    //           </View>
+    //         );
+    //       })} */}
+    //   </View>
+    // );
   };
-  return <View>{renderQuestion()}</View>;
+  return (
+    <View style={styles.container}>
+      <ScrollView>{renderQuestion()}</ScrollView>
+      <FlatList
+        data = {dataQuestion}
+        renderItem={({ item }) => renderQuestion(item)}
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    marginHorizontal : 10,
+  },
+  viewQuestion : {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems:'center',
+  }
+});
 
 export default App;
